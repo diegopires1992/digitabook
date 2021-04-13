3f24fae67f3bd79930ed6a60f080195f853c0b68
Emanuel <emanuel.gotstark@gmail.com>
2021-04-13 15:27:19 -0300

    feat: set configurations

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from os import getenv
+
+from config import config_selector
+from app.configurations import database, migration
+from app import views
+
+
+def create_app():
+    app = Flask(__name__)
+
+    config_type = getenv("FLASK_ENV")
+    app.config.from_object(config_selector[config_type])
+
+    database.init_app(app)
+    migration.init_app(app)
+    views.init_app(app)
+
+    return app
