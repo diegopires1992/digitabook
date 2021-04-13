20ef471bcca56cb8cfc16a61a7e9e297a78d63cb
Emanuel <emanuel.gotstark@gmail.com>
2021-04-13 19:39:17 -0300

    feat: created the route of authors and adjustments in the settings

--- app/models/authors_model.py
+++ app/models/authors_model.py
@@ -1,9 +1,9 @@
 from . import db
 
 
-class AuthorsModel(db.Model):
+class AuthorModel(db.Model):
     __tablename__ = 'authors'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullabe=False)
+    name = db.Column(db.String, nullable=False)
     birthplace = db.Column(db.String, nullable=False)
