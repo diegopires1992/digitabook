174f64092063d9cc7afb60923f6f69094423c6ef
Bruno Gehlen <tousepc@gmail.com>
2021-04-13 17:43:52 -0300

    Feat: Add product model non relational properties

---
+++ app/models/product_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import backref
+
+from . import db
+
+class ProductModel(db.Model):
+    
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    subtitle = db.Column(db.String, nullable=True)
+    publisher = db.Column(db.String, nullable=False)
+    isbn13 = db.Column(db.String, nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String, nullable=True)
