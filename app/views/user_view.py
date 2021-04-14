840306e502edfc4ad51badd4f7ee37b13adaa194
Eliabe Santos <eliabedodo@gmail.com>
2021-04-14 10:54:16 -0300

    feat:crud user view delete included

--- app/views/user_view.py
+++ app/views/user_view.py
@@ -13,13 +13,29 @@ def list_users(id):
 
     print(found_user.__dict__)
 
-    return {"user": found_user.name}
+    return {"user": found_user.name,
+            "email": found_user.email}
+
+@bp_user.route("/<int:id>", methods=["DELETE"])
+def delete_user(id):
+    session = current_app.db.session
+
+    found_user: UserModel = UserModel.query.get_or_404(id)
+    
+    print(found_user.__dict__)
+
+    session.delete(found_user)
+    session.commit()
+
+    return "usuario deletado",HTTPStatus.NO_CONTENT
+
 
 @bp_user.route("/register", methods=["POST"])
 def create_user():
     session = current_app.db.session
 
     data = request.get_json()
+
     profile = UserModel(
         name=data["name"],
         email=data["email"],
