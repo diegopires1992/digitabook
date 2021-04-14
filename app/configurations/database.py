823cd7034be30a978dcf608c2904c3f30f562caf
Leandro Lino <leao.lino7@gmail.com>
2021-04-14 15:32:56 -0300

    feature: add db commits

--- app/configurations/database.py
+++ app/configurations/database.py
@@ -8,6 +8,14 @@ def init_app(app: Flask):
     db.init_app(app)
     app.db = db
 
+    
+
     from app.models.authors_model import AuthorModel
     from app.models.authors_products_model import AuthorsProducts
     from app.models.product_model import ProductModel
+
+    context = app.app_context()
+    context.push()
+    db.create_all()
+    db.session.commit()
+    context.pop()
