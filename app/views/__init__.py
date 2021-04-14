8cad9d769e3f05d2f4908cc048d2d896b752b109
Bruno Gehlen <tousepc@gmail.com>
2021-04-14 12:02:00 -0300

    Feat: Add products modelblueprint to app package initialization

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,3 +4,6 @@ from flask import Flask
 def init_app(app: Flask):
     from app.views.authors_view import bp_authors
     app.register_blueprint(bp_authors)
+
+    from app.views.products_view import bp_products
+    app.register_blueprint(bp_products)
