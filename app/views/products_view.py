82a629f3caf7be0c8b307652761f1560475c546a
Bruno Gehlen <tousepc@gmail.com>
2021-04-14 12:03:31 -0300

    Feat: Add base /products route for method GET, to retrieve all products

--- app/views/products_view.py
+++ app/views/products_view.py
@@ -1,5 +1,21 @@
 from flask import Blueprint, request, current_app
 from http import HTTPStatus
 from app.models.product_model import ProductModel
+from app.models.authors_model import AuthorModel
 
 bp_products = Blueprint('products_view', __name__, url_prefix='/products')
+
+@bp_products.route('/', methods=['GET'])
+def get_products():
+    
+    session = current_app.db.session
+    products = ProductModel.query.all()
+
+    return {
+        "products": [{
+            "title": product.title, 
+            "author_list": [{
+                "name": author.name
+                } for author in product.author_list
+            ]} for product in products
+    ]}, HTTPStatus.OK
