75286ce23b4d53d7136bb837a61553e1d43903ef
Leandro Lino <leao.lino7@gmail.com>
2021-04-14 20:50:18 -0300

    Feature: maked a route of update and delete

--- app/views/products_view.py
+++ app/views/products_view.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from app.models.product_model import ProductModel
 from app.models.authors_model import AuthorModel
 from sqlalchemy import update
+import ipdb
 
 bp_products = Blueprint('products_view', __name__, url_prefix='/products')
 
@@ -31,12 +32,9 @@ def get_products_id(id_prod):
     return {'id':products.id,
             'title':products.title,
             'subtitle':products.subtitle,
-            'publisher':products.publisher,
             'isbn13':products.isbn13,
-            'pages':products.pages,
-            'description':products.description,
             'price':products.price,
-            'image_url':products.image_url}, HTTPStatus.OK
+            'image_url':products.image}, HTTPStatus.OK
 
 @bp_products.route('/<int:id_prod>', methods=['DELETE'])
 def delete_products_id(id_prod):
@@ -54,17 +52,18 @@ def delete_products_id(id_prod):
 @bp_products.route('/<int:id_prod>', methods=['PATCH'])
 def path_products_id(id_prod):
 
-    body = request.get_json()
-
     session = current_app.db.session
+    found_user: ProductModel = ProductModel.query.get(id_prod)
+    data = request.get_json()
+    print(data)
 
+    try:
+        for key, value in data.items():
+            setattr(found_user, key, value)
+        session.add(found_user)
+        session.commit()
 
-
-    product_to_update = ProductModel.query.get(id_prod)
-    product_to_update.query.update(body)
-
-    session.add(product_to_update)
-
-    session.commit()
-
-    return {'data':'deleted'}, HTTPStatus.OK
+        return {"user": 'found_user.name', "email": 'found_user.email'},HTTPStatus.OK
+    except Exception as nome:
+        print(nome)
+        return "test",HTTPStatus.BAD_REQUEST
