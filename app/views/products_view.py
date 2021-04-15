539eec903c45948a9a2fb6bf5bf3addbec47466d
Bruno Gehlen <tousepc@gmail.com>
2021-04-14 23:12:45 -0300

    Fix: Fix indentation and removed unused code

--- app/views/products_view.py
+++ app/views/products_view.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from app.models.product_model import ProductModel
 from app.models.authors_model import AuthorModel
 from sqlalchemy import update
-import ipdb
 
 bp_products = Blueprint('products_view', __name__, url_prefix='/products')
 
@@ -14,10 +13,10 @@ def get_products():
     products = ProductModel.query.all()
     session.commit()
     return {
-        "products": [{
-            "title": product.title, 
-            "author_list": [{
-                "name": author.name
+        'products': [{
+            'title': product.title, 
+            'author_list': [{
+                'name': author.name
                 } for author in product.author_list
             ]} for product in products
     ]}, HTTPStatus.OK
@@ -27,21 +26,23 @@ def get_products():
 def get_products_id(id_prod):
     
     session = current_app.db.session
-    products = ProductModel.query.get(id_prod)
+    found_product: ProductModel = ProductModel.query.get(id_prod)
     session.commit()
-    return {'id':products.id,
-            'title':products.title,
-            'subtitle':products.subtitle,
-            'isbn13':products.isbn13,
-            'price':products.price,
-            'image_url':products.image}, HTTPStatus.OK
+
+    return {'id': found_product.id,
+            'title': found_product.title,
+            'subtitle': found_product.subtitle,
+            'isbn13': found_product.isbn13,
+            'price': found_product.price,
+            'image_url': found_product.image
+        }, HTTPStatus.OK
 
 @bp_products.route('/<int:id_prod>', methods=['DELETE'])
 def delete_products_id(id_prod):
     
     session = current_app.db.session
     try:
-        product_to_delete = ProductModel.query.get(id_prod)
+        product_to_delete: ProductModel = ProductModel.query.get(id_prod)
         deleted = current_app.db.session.delete(product_to_delete)
     except:
         return {'result': 'Not Found'},HTTPStatus.NOT_FOUND
@@ -55,7 +56,6 @@ def path_products_id(id_prod):
     session = current_app.db.session
     found_user: ProductModel = ProductModel.query.get(id_prod)
     data = request.get_json()
-    print(data)
 
     try:
         for key, value in data.items():
@@ -65,5 +65,5 @@ def path_products_id(id_prod):
 
         return {"user": 'found_user.name', "email": 'found_user.email'},HTTPStatus.OK
     except Exception as nome:
-        print(nome)
         return "test",HTTPStatus.BAD_REQUEST
+
