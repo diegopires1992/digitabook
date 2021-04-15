b5e4661762173a7b5d3ae800258dab20e5e331eb
Bruno Gehlen <tousepc@gmail.com>
2021-04-14 23:46:08 -0300

    Feat: Implementing Services layer to get all products, route base url GET request

--- app/views/products_view.py
+++ app/views/products_view.py
@@ -3,23 +3,14 @@ from http import HTTPStatus
 from app.models.product_model import ProductModel
 from app.models.authors_model import AuthorModel
 from sqlalchemy import update
+from app.services.products_services import ProductServices
+import ipdb
 
 bp_products = Blueprint('products_view', __name__, url_prefix='/products')
 
 @bp_products.route('/', methods=['GET'])
 def get_products():
-    
-    session = current_app.db.session
-    products = ProductModel.query.all()
-    session.commit()
-    return {
-        'products': [{
-            'title': product.title, 
-            'author_list': [{
-                'name': author.name
-                } for author in product.author_list
-            ]} for product in products
-    ]}, HTTPStatus.OK
+    return ProductServices.get_all_products(), HTTPStatus.OK
 
 
 @bp_products.route('/<int:id_prod>', methods=['GET'])
