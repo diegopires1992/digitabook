9d301dfdaa4d0899df88b3bb93d1f38ba6f33109
Bruno Gehlen <tousepc@gmail.com>
2021-04-14 23:51:16 -0300

    Feat: Add @staticmethod to retrieve all products from /products

--- app/services/products_services.py
+++ app/services/products_services.py
@@ -16,3 +16,27 @@ class ProductServices:
         
         session.add(new_book)
         session.commit()
+
+    @staticmethod
+    def get_all_products():
+        
+        session = current_app.db.session
+        products = ProductModel.query.all()
+        session.commit()
+
+        return {
+            'products': [{
+                'id':product.id,
+                'title': product.title,
+                'subtitle': product.subtitle,
+                'isbn13': product.isbn13,
+                'price': product.price,
+                'image_url': product.image, 
+                'authors': [{
+                    'name': author.name,
+                    'birthplace': author.birthplace,
+                    'book_list_url': f'http://deploy.url/products/?author_id={author.id}'
+                    } for author in product.author_list
+                ]} for product in products
+            ]
+        }
