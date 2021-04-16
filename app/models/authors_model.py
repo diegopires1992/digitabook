2ac475563957b76a952d98575f29be47aeb42afc
Leandro Lino <leao.lino7@gmail.com>
2021-04-16 19:19:06 -0300

    Feature: added unique in authors model in name property

--- app/models/authors_model.py
+++ app/models/authors_model.py
@@ -5,5 +5,5 @@ class AuthorModel(db.Model):
     __tablename__ = 'authors'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     birthplace = db.Column(db.String, nullable=False)
