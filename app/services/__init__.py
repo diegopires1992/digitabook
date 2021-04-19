5d1c50a255f6aa9b5b3c3c51fc397a8408759822
Leandro Lino <leao.lino7@gmail.com>
2021-04-19 10:34:28 -0300

    Fix: Fixed import of AuthorsSchema

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,6 +2,6 @@ from app.models.authors_model import AuthorModel
 from app.models.authors_products_model import AuthorsProducts 
 from app.models.product_model import ProductModel
 from app.serializer.products_schema import ProductAuthorSchema
-from app.serializer.products_schema import AuthorSchema
+from app.serializer.authors_schema import AuthorSchema
 from flask import Flask, current_app
 from http import HTTPStatus
