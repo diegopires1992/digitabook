e5ad33acff790dc4114047212e669869f3232c2d
Leandro Lino <leao.lino7@gmail.com>
2021-04-19 10:33:58 -0300

    Fix: Removed AuthorsSchemas

--- app/serializer/products_schema.py
+++ app/serializer/products_schema.py
@@ -6,35 +6,6 @@ from . import (
 )
 
 
-class AuthorSchema(marsh.SQLAlchemyAutoSchema):
-    class Meta:
-        model = AuthorModel
-        fields = (
-            'id',
-            'name',
-            'birthplace'
-        )
-        ordered = True
-        load_instance = True
-
-    id = marsh.auto_field()
-    name = marsh.auto_field()
-    birthplace = marsh.auto_field()
-
-
-    def author_not_found(self):
-        return { 'Message': 'Author not found' }
-
-
-    def author_not_deleted(self):
-        return { 'Message': 'deleted not author' }
-
-
-    def author_exists(self):
-        return {"message":"Author already exists"}
-
-
-
 class ProductAuthorSchema(marsh.SQLAlchemyAutoSchema):
     class Meta:
         model = ProductModel
